12a0ed3ba07f87db9ea32ad9c3ac45a07df263c9
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-28 14:38:12 +0530

    Update 111.c

--- 111.c
+++ 111.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <string.h>
- 
-/* Function Prototype */
+#inclued,conio.h>
 void swap(int*, int *);
- 
 void main()
 {
     int num1, num2;
@@ -13,8 +11,6 @@ void main()
     swap(&num1, &num2);        /* Call by Reference to function swap */
     printf("\nThe numbers after swapping are Number1= %d Number2 = %d", num1, num2);
 }
- 
-/* Code to swap two numbers using bitwise operator */
 void swap(int *x, int *y)
 {
     *x = *x ^ *y;
