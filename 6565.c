7aa940190beb519da80d13e8323ef6461e241d7f
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-28 14:41:11 +0530

    Update 6565.c

--- 6565.c
+++ 6565.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#include<conio.h>
   int main() {
     int a[10];
     int i;
     int greatest;
     printf("Enter ten values:");
-    //Store 10 numbers in an array
     for (i = 0; i < 10; i++) {
     scanf("%d", &a[i]);
     }
-    //Assume that a[0] is greatest
     greatest = a[0];
     for (i = 0; i < 10; i++) {
 if (a[i] > greatest) {
