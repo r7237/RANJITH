b2e765f715e52cafa547aa3b82466f5da64d2861
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-28 14:45:25 +0530

    Update 1212.c

--- 1212.c
+++ 1212.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
+#include<conio.h>
 int main()
 {
     int i, n, t1 = 0, t2 = 1, nextTerm;
-
     printf("Enter the number of terms: ");
     scanf("%d", &n);
-
     printf("Fibonacci Series: ");
-
     for (i = 1; i <= n; ++i)
     {
         printf("%d, ", t1);
