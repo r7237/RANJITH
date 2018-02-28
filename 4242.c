24d47561dabd4a4d05b5875df7efac1b9c31e7e2
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-28 14:24:53 +0530

    Update 4242.c

--- 4242.c
+++ 4242.c
@@ -1,19 +1,12 @@
 include <stdio.h>
-
+#include<conio.h>
 int main()
 {
     int i, n;
-
-    /* Input upper limit of even number from user */
     printf("Print all even numbers till: ");
     scanf("%d", &n);
 
-    printf("All even numbers from 1 to %d are: \n", n);
-
-    /*
-     * Start loop from 2 and increment by 2, 
-     * in each repetition
-     */
+    printf("All even numbers from 1 to %d are);
     for(i=2; i<=n; i+=2)
     {
         printf("%d\n",i);
