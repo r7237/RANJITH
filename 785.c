b950b8e915cffaa4b3da94971cd15218abdb6f0d
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-28 14:32:16 +0530

    Update 785.c

--- 785.c
+++ 785.c
@@ -1,10 +1,8 @@
- */
 #include <stdio.h>
 void main()
 {
     char string[80];
-    int count, nc = 0, sum = 0;
- 
+    int count, nc = 0, sum = 0; 
     printf("Enter the string containing both digits and alphabet \n");
     scanf("%s", string);
     for (count = 0; string[count] != '\0'; count++)
